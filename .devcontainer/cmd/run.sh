953e5ffda2e2a7f667ff345d9753415fe07cf30e
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-09-09 17:38:09 -0300

    wasm-pack instalation on the Dockerfile

--- .devcontainer/cmd/run.sh
+++ .devcontainer/cmd/run.sh
@@ -1,7 +1,7 @@
 # rust wasm build
 echo "Building rust..."
 wasm-pack build --target web --out-dir ./public/script
-wasm-opt -Os ./public/script/chrome_dino_game_bg.wasm -o ./public/script/chrome_dino_game_bg.wasm
+#wasm-opt -Os ./public/script/chrome_dino_game_bg.wasm -o ./public/script/chrome_dino_game_bg.wasm
 echo "Done!"
 
 cd ./src
